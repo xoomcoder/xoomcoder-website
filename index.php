38e7295021fb428cd0fad5042f77e33d5391a14e
LH XoomCoder <github@xoomcoder.com>
2020-09-07 13:11:29 +0200

    ajout test

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 
 $uri = $_SERVER["REQUEST_URI"];
-echo $uri;
+echo "($uri)";
 
 // log
-$today = date("Y-m-d");
+if (!empty($_REQUEST)) {
+    $today = date("Y-m-d");
+    $logdata = json_encode($_REQUEST, JSON_PRETTY_PRINT);
+    file_put_contents(__DIR__ . "/log/request-$today.log", "$logdata\n", FILE_APPEND);    
+}
 
-$logdata = json_encode($_REQUEST, JSON_PRETTY_PRINT);
-
-file_put_contents(__DIR__ . "/log/request-$today.log", "$logdata\n", FILE_APPEND);
-
+// git pull
 if ($uri == "/gitpull") {
     $commande = "git pull";
     passthru($commande);
