e3f3afd8c50c842108e8468a6ef22af4729a0494
LH XoomCoder <github@xoomcoder.com>
2020-09-07 16:32:50 +0200

    studio v0.0.1

--- templates/pages/vuejs-studio.php
+++ templates/pages/vuejs-studio.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- IMPORTANT: NO INDEX -->
+    <meta name="robots" content="noindex">
+    <!-- favicon -->
+    <link rel="icon" href="assets/img/xoomcoder.svg">
+
     <title>XoomCoder Studio</title>
 
     <style>
@@ -18,8 +23,12 @@ html, body {
     box-sizing: border-box;
     width:100%;
     max-width:100%;
+    transition: all 0.5s ease-out;
 }
-
+a {
+    text-decoration: none;
+    color: #aaaaaa;
+}
 p, pre {
     white-space: pre-wrap;
 }
@@ -60,83 +69,102 @@ section {
         width: calc(100% / 4 - 0.5rem); /* 4 columns and remove 2x margin */
     }
 
-}        
+}     
+
+/* toolbar */
+img.action {
+    width:2rem;
+    height:2rem;
+    cursor:pointer;
+}
+.action.settings {
+    position:fixed;
+    top:2rem;
+    right:2rem;
+}
+.options {
+    position:fixed;
+    top:100%;
+    right:0;
+    background-color: rgba(0,0,0,.9);
+    height:100%;
+    padding:1rem;
+}
+.options.active {
+    top:0;
+}
+.options .panel {
+    background-color: #dddddd;
+    padding:1rem;
+}
     </style>
 </head>
 <body>
     <div class="page">
         <header>
-            <h1>SITE TITLE</h1>
+            <h1><a href="">XoomCoder Studio</a></h1>
+            <h6><a href="./">revenir sur le site</a></h6>
         </header>
         <main>
             <section v-for="section in sections">
                 <h2>{{ section.title }}</h2>
                 <article v-for="article in section.articles">
                     <h3>{{ article.title }}</h3>
-                </article>
-            </section>
-            <section>
-                <h1>TITLE1 SECTION</h1>
-                <article>
-                    <h2>title2 article1</h2>
-                    <p>content article1</p>
-                </article>
-                <article>
-                    <h2>title2 article2</h2>
-                    <p>content article2</p>
-                </article>
-                <article>
-                    <h2>title2 article3</h2>
-                    <p>content article3</p>
-                </article>
-                <article>
-                    <h2>title2 article4</h2>
-                    <p>content article4</p>
-                </article>
-                <article>
-                    <h2>title2 article5</h2>
-                    <p>content article5</p>
-                </article>
-                <article>
-                    <h2>title2 article6</h2>
-                    <p>content article6</p>
+                    <p>{{ article.content }}</p>
                 </article>
             </section>
         </main>
+        <div :class="{ 'options': true, 'active': show.options }">
+            <div class="panel">
+                <h2>options</h2>
+            </div>
+        </div>
         <footer>
+            <div class="toolbar">
+                <img @click="switchOptions" class="action settings" src="assets/img/settings.svg" alt="settings">
+            </div>
             <p>all rights reserved - © 2020 - {{ debug }}</p>
         </footer>
-    </div>
+
+    </div><!--/.page-->
 
     <script type="module">
 // load Vue from module        
 import * as Vue from 'https://cdn.jsdelivr.net/npm/vue@3.0.0-rc.1/dist/vue.esm-browser.js';
 
-const appconf = {
-  data() {
-    return {
-        sections: [
-            { title: 'section1', articles: [
-                { title: 'article1' },
-                { title: 'article2' },
-                { title: 'article3' },
-                { title: 'article4' },
-                { title: 'article5' },
-                { title: 'article6' },
-            ] },
-            { title: 'section2', articles: [
-                { title: 'article1' },
-                { title: 'article2' },
-                { title: 'article3' },
-            ]},
-            { title: 'section3', articles: [
-                { title: 'article1' },
-                { title: 'article2' },
-                { title: 'article3' },
-            ] }
-        ],
-        debug: 'xoomcoder.com'
+const mymethods = {
+    switchOptions () {
+        this.show.options = !this.show.options;
     }
+};
+
+const appconf = {
+    methods: mymethods,
+    data() {
+        return {
+            show: { options: false },
+            sections: [
+                { title: 'section1', articles: [
+                    { title: 'article1', content: 'contenu1' },
+                    { title: 'article2', content: 'contenu2' },
+                    { title: 'article3', content: 'contenu3' },
+                    { title: 'article4', content: 'contenu4' },
+                    { title: 'article5', content: 'contenu5' },
+                    { title: 'article6', content: 'contenu6' },
+                ] },
+                { title: 'section2', articles: [
+                    { title: 'article1' },
+                    { title: 'article2' },
+                    { title: 'article3' },
+                ]},
+                { title: 'section3', articles: [
+                    { title: 'article1' },
+                    { title: 'article2' },
+                    { title: 'article3' },
+                ] }
+            ],
+            debug: 'xoomcoder.com'
+        }
   }
 }
 
