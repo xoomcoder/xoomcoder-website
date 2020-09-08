1575f1375df2fb04bbf49903a68668190614a909
LH XoomCoder <github@xoomcoder.com>
2020-09-08 17:13:51 +0200

    ajout page emploi

---
+++ templates/content/emploi.php
@@ -0,0 +1,5 @@
+
+<section>
+    <h1>Recherche et Offres d'Emploi</h1>
+    <p>Sur cette page, vous pourrez trouver des offres d'emploi géolocalisées et aussi publier votre annonce de recherche d'emploi.</p>
+</section>
