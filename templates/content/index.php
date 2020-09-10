97c186e57a926fe84046501073108ae76b60df51
LH XoomCoder <github@xoomcoder.com>
2020-09-10 18:22:02 +0200

    retrait form google forms

--- templates/content/index.php
+++ templates/content/index.php
@@ -214,11 +214,3 @@ Des modules supplémentaires sont aussi disponibles
             </article>
         </section>
 
-        <section class="nobg">
-            <h2>Formulaire de contact</h2>
-            <strong>Remplissez le formulaire suivant et recevez un code promo de 20% sur votre prochain achat sur le site.</strong>
-            <script type="text/xoomcoder">
-                <iframe title="formulaire de contact" src="https://docs.google.com/forms/d/e/1FAIpQLSdz3ZjxG5lYBoV7vwnab00pnldHRl92tRVq9ydH-0WCeIiMug/viewform?embedded=true" width="10%" height="3600" frameborder="0" marginheight="0" marginwidth="0">Chargement…</iframe>   
-            </script>
-        </section>
-
