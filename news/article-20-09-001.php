001a6004847ba11e6808ff1e138137a20e676d4f
LH XoomCoder <github@xoomcoder.com>
2020-09-10 18:20:00 +0200

    ajout article avec bloc md

--- news/article-20-09-001.php
+++ news/article-20-09-001.php
@@ -1,23 +1,4 @@
 <article>
-    <h2>Coronavirus, Data et analyses</h2>
-    <p>
-    L'Open Data permet de créer des analyses précises si les outils sont déjà disponibles.
-    La France a découvert en temps de crise l'utilité des ces outils qui expliquent et éclairent sur l'évolution rapide d'une épidémie. L'article retrace comment ce sont des volontaires qui ont réussi à développer des outils pour mieux capter les données journalières.
-    </p>
-    <p>Comme extraits révélateurs de la situation:</p>
-    <p>
-    "En France, on a une culture du communiqué de presse, pas une culture de la donnée. Pourtant, c'est essentiel pour éclairer la décision publique et l'opinion", estime-t-il. Un autre, agent d'une ARS, témoignait anonymement auprès de Mediapart : "On travaille avec des outils archaïques, on a beaucoup bricolé à base d’Excel et de copier-coller, on a perdu des journées et des soirées à refaire nos tableaux à la main. Nous sommes bien loin de la start-up nation."
-    </p>
-    <p>https://www.francetvinfo.fr/sante/maladie/coronavirus/urgence-cafouillages-mille-feuille-d-indicateurs-dans-les-coulisses-des-donnees-tres-strategiques-sur-le-covid-19_4099757.html</p>
-    <p>
-    Le code source est sur GitHub.com
-    </p>
-    <p>
-    https://github.com/etalab/covid19-dashboard
-    </p>
-</article>
-
-<article>
     <h2>Level 2: header avec nav</h2>
     <p>
     Pour utiliser Grid CSS, une des limitations de la structure HTML est d'avoir un container. Et seulement les balises enfants pourront être disposés suivant un layout grid.
