1cb9554e100bd19b16d08a0bcb758d981fb3dd46
LH XoomCoder <github@xoomcoder.com>
2020-09-13 19:56:45 +0200

    news-02

--- templates/content/news-02.php
+++ templates/content/news-02.php
@@ -2,12 +2,15 @@
 <section class="x3col">
     <h1>News</h1>
     <a class="cta w100" href="inscription" title="REJOIGNEZ LA COMMUNAUTE XOOMCODER">Rejoignez la communauté XoomCoder&nbsp;!</a>
-    <nav>
+
+    <nav class="w100">
         <a href="news">page 1</a>
         <a href="news-02">page 2</a>
     </nav>
+
     <?php News::show() ?>
-    <nav>
+
+    <nav class="w100">
         <a href="news">page 1</a>
         <a href="news-02">page 2</a>
     </nav>
