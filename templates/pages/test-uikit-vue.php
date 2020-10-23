7e87bf1ed51e12a2f5fe7cc27388dd6974b4ee3d
LH XoomCoder <github@xoomcoder.com>
2020-10-23 20:34:21 +0200

    add content

--- templates/pages/test-uikit-vue.php
+++ templates/pages/test-uikit-vue.php
@@ -20,12 +20,15 @@
                 <div class="uk-container">
                     <h2>sortable list</h2>
                     <div class="uk-child-width-1-2@s uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid uk-sortable>
-                        <article class="uk-card" v-for="item in items" :key="item.id">
+                        <article class="uk-card" v-for="(item, index) in items" :key="item.id">
                             <div class="uk-card-header">
                                 <h3>{{ item.title }}</h3>
                             </div>
-                            <div class="uk-card-body">
-                                <img src="assets/square/team.jpg" alt="team">
+                            <div class="uk-card-body" uk-lightbox>
+                                <a :href="'assets/square/team-' + index + '.jpg'">
+                                    <img :src="'assets/square/team-' + index + '.jpg'" alt="team">
+                                </a>
+                                <p>{{ item.code }}</p>
                             </div>
                         </article>
                     </div>
@@ -45,17 +48,36 @@
         const appconf = {
             data() {
                 return {
-                    items: [{
+                    items: [
+                        {
                             id: 1,
-                            title: 'title 1'
+                            title: 'title 1',
+                            code: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis commodi maxime, accusantium unde necessitatibus aperiam ipsa ut voluptatem fugit. Cum exercitationem dolorem inventore nisi modi unde quaerat obcaecati minus animi.'
                         },
                         {
                             id: 2,
-                            title: 'title 2'
+                            title: 'title 2',
+                            code: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis commodi maxime, accusantium unde necessitatibus aperiam ipsa ut voluptatem fugit. Cum exercitationem dolorem inventore nisi modi unde quaerat obcaecati minus animi.'
                         },
                         {
                             id: 3,
-                            title: 'title 3'
+                            title: 'title 3',
+                            code: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis commodi maxime, accusantium unde necessitatibus aperiam ipsa ut voluptatem fugit. Cum exercitationem dolorem inventore nisi modi unde quaerat obcaecati minus animi.'
+                        },
+                        {
+                            id: 4,
+                            title: 'title 4',
+                            code: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis commodi maxime, accusantium unde necessitatibus aperiam ipsa ut voluptatem fugit. Cum exercitationem dolorem inventore nisi modi unde quaerat obcaecati minus animi.'
+                        },
+                        {
+                            id: 5,
+                            title: 'title 5',
+                            code: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis commodi maxime, accusantium unde necessitatibus aperiam ipsa ut voluptatem fugit. Cum exercitationem dolorem inventore nisi modi unde quaerat obcaecati minus animi.'
+                        },
+                        {
+                            id: 6,
+                            title: 'title 6',
+                            code: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis commodi maxime, accusantium unde necessitatibus aperiam ipsa ut voluptatem fugit. Cum exercitationem dolorem inventore nisi modi unde quaerat obcaecati minus animi.'
                         },
                     ]
                 }
