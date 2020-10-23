1938e11b6a944bb2813e4dab0df9df9bf6a373f6
LH XoomCoder <github@xoomcoder.com>
2020-10-23 19:29:57 +0200

    add uikit3 and vue3

---
+++ templates/pages/test-uikit-vue.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="assets/uikit/css/uikit.css">
+</head>
+
+<body>
+    <div id="app">
+        <header class="uk-container">
+            <h1>UIKIT AND VUEJS</h1>
+        </header>
+        <main>
+            <section class="uk-section">
+                <div class="uk-container" uk-sortable>
+                    <div v-for="item in items" :key="item.id">{{ item.title }}</div>
+                </div>
+            </section>
+
+        </main>
+        <footer class="uk-container">
+            <p>tous droits réservés</p>
+        </footer>
+    </div>
+
+    <script src="assets/js/vue.global.js"></script>
+    <script src="assets/uikit/js/uikit.js"></script>
+    <script src="assets/uikit/js/uikit-icons.js"></script>
+    <script>
+        const appconf = {
+            data() {
+                return {
+                    items: [{
+                            id: 1,
+                            title: 'title 1'
+                        },
+                        {
+                            id: 2,
+                            title: 'title 2'
+                        },
+                        {
+                            id: 3,
+                            title: 'title 3'
+                        },
+                    ]
+                }
+            }
+        }
+
+        Vue.createApp(appconf).mount('#app')
+    </script>
+</body>
+
+</html>
