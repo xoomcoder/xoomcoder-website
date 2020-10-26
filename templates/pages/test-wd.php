b60572130512719ed94438e74febf07afab94686
LH XoomCoder <github@xoomcoder.com>
2020-10-26 19:33:41 +0100

    reset avec uikot +vue

--- templates/pages/test-wd.php
+++ templates/pages/test-wd.php
@@ -9,12 +9,7 @@
     <title>XoomCoder.com Formation Dev FullStack</title>
     <link rel="icon" href="assets/img/xoomcoder.svg">
 
-    <!-- preload -->
-    <!-- <link rel="preload" href="assets/uikit/css/uikit.min.css" as="style"> -->
-    <link rel="preload" href="assets/js/vue.global.prod.js" as="script">
-    <link rel="preload" href="assets/uikit/js/uikit.min.js" as="script">
-    <link rel="preload" href="assets/uikit/js/uikit-icons.min.js" as="script">
-
+    <link rel="stylesheet" href="assets/uikit/css/uikit.min.css">
     <style>
         /* variables */
         :root {
@@ -29,110 +24,7 @@
             --grey: #aaaaaa;
         }
 
-        /* html,
-        body {
-            font-size: 16px;
-            margin: 0;
-            padding: 0;
-        }
-
-        * {
-            box-sizing: border-box;
-        } */
-
-        header {
-            position: relative;
-        }
-
-        header .title {
-            font-size: 2rem;
-            text-align: center;
-            padding: 1rem;
-            font-weight: 900;
-        }
-
-        /* h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6,
-        footer {
-            text-align: center;
-            margin: 0;
-            padding: 1rem;
-        } */
-
-        /* a {
-            text-decoration: none;
-            color: #333333;
-        } */
-
-        nav {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-            position: sticky;
-            top: 0;
-            left: 0;
-        }
-
-        nav a {
-            max-width: 160px;
-            padding: 1rem;
-        }
-
-        /* p {
-            padding: 1rem;
-        } */
-
-        img.h30 {
-            object-fit: cover;
-            width: 100%;
-            height: 30vmax;
-            background-color: #666666;
-        }
-
-        .logo {
-            width: 4rem;
-            height: 4rem;
-            object-fit: contain;
-            /* position: absolute; */
-            padding: 1rem;
-            background-color: transparent;
-        }
-
-        /* colors */
-        /* header .title {
-            background-color: var(--primary);
-        } */
-
-        nav {
-            background-color: var(--light);
-            box-shadow: 2px 4px 8px var(--grey);
-        }
-
-        /* h2 {
-            background-color: var(--secondary);
-        }
-
-        h3 {
-            background-color: var(--secondary-light);
-        } */
-
-        nav a {
-            color: var(--primary-dark);
-        }
-
-        #debug {
-            position: fixed;
-            bottom: 0;
-            left: 0;
-        }
     </style>
-
-    <link rel="stylesheet" href="assets/uikit/css/uikit-1.css">
-
     <script>
         let mypage = {};
         // web.dev test screen: 360x640
@@ -145,7 +37,7 @@
     <div class="page">
         <header>
             <img class="logo" src="assets/img/xoomcoder.svg" alt="logo">
-            <a class="title" href="#">XoomCoder</a>
+            <div class="title"><a href="#">XoomCoder</a></div>
         </header>
         <nav>
             <a href="#">news</a>
@@ -157,7 +49,7 @@
         <main>
             <section>
                 <h1><a href="#">Formation Dev FullStack</a></h1>
-                <img class="h30" loading="lazy" src="assets/img/team-640.jpg" alt="team">
+                <img loading="lazy" src="assets/img/team-640.jpg" alt="team">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo laboriosam obcaecati sint iure illo, nisi facilis nulla odit in error dicta sequi doloremque voluptas aliquam officia animi debitis reiciendis?</p>
                 <article>
                     <h2><a href="#">Formation à Distance</a></h2>
@@ -195,13 +87,10 @@
             <p>tous droits réservés</p>
         </footer>
         <div id="debug"></div>
-
-        <!-- <script src="assets/js/vue.global.prod.js"></script> -->
-
         <script src="assets/uikit/js/uikit.min.js"></script>
-        <script src="assets/uikit/js/uikit-icons.min.js"></script>
+    <script src="assets/uikit/js/uikit-icons.min.js"></script>
 
-
+    <script src="assets/js/vue.global.prod.js"></script>
         <script>
             // debug.innerHTML = mypage.width + '/' + mypage.height;
         </script>
