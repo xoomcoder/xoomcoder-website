7a3494b65e8773a5dc05200256b3e037e3e706ee
LH XoomCoder <github@xoomcoder.com>
2020-10-26 19:41:15 +0100

    add menu aside off-canvas

--- templates/pages/test-wd.php
+++ templates/pages/test-wd.php
@@ -23,7 +23,6 @@
             --dark: #000000;
             --grey: #aaaaaa;
         }
-
     </style>
     <script>
         let mypage = {};
@@ -38,62 +37,74 @@
         <header>
             <img class="logo" src="assets/img/xoomcoder.svg" alt="logo">
             <div class="title"><a href="#">XoomCoder</a></div>
+            <div class="uk-navbar-container" uk-navbar>
+                <div class="uk-navbar-left">
+                    <a class="uk-navbar-toggle" uk-navbar-toggle-icon href="#aside-panel"></a>
+                </div>
+            </div>
         </header>
-        <nav>
-            <a href="#">news</a>
-            <a href="#">tutoriels</a>
-            <a href="#">formation</a>
-            <a href="#">offres d'emploi</a>
-            <a href="#">contact</a>
-        </nav>
-        <main>
-            <section>
-                <h1><a href="#">Formation Dev FullStack</a></h1>
-                <img loading="lazy" src="assets/img/team-640.jpg" alt="team">
+        <aside id="aside-panel" uk-offcanvas>
+            <div class="uk-offcanvas-bar">
+
+                <button class="uk-offcanvas-close" type="button" uk-close></button>
+                <nav>
+                    <a href="#">news</a>
+                    <a href="#">tutoriels</a>
+                    <a href="#">formation</a>
+                    <a href="#">offres d'emploi</a>
+                    <a href="#">contact</a>
+                </nav>
+
+        </aside>
+    </div>
+    <main>
+        <section>
+            <h1><a href="#">Formation Dev FullStack</a></h1>
+            <img loading="lazy" src="assets/img/team-640.jpg" alt="team">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo laboriosam obcaecati sint iure illo, nisi facilis nulla odit in error dicta sequi doloremque voluptas aliquam officia animi debitis reiciendis?</p>
+            <article>
+                <h2><a href="#">Formation à Distance</a></h2>
+                <img loading="lazy" src="assets/img/code-640.jpg" alt="team">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo laboriosam obcaecati sint iure illo, nisi facilis nulla odit in error dicta sequi doloremque voluptas aliquam officia animi debitis reiciendis?</p>
-                <article>
-                    <h2><a href="#">Formation à Distance</a></h2>
-                    <img loading="lazy" src="assets/img/code-640.jpg" alt="team">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo laboriosam obcaecati sint iure illo, nisi facilis nulla odit in error dicta sequi doloremque voluptas aliquam officia animi debitis reiciendis?</p>
-                </article>
-            </section>
-            <section>
-                <h2><a href="#">Développeur Web FullStack</a></h2>
-                <img loading="lazy" src="assets/img/team-640.jpg" alt="team">
+            </article>
+        </section>
+        <section>
+            <h2><a href="#">Développeur Web FullStack</a></h2>
+            <img loading="lazy" src="assets/img/team-640.jpg" alt="team">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo laboriosam obcaecati sint iure illo, nisi facilis nulla odit in error dicta sequi doloremque voluptas aliquam officia animi debitis reiciendis?</p>
+            <article>
+                <h3><a href="#">Front</a></h3>
+                <img loading="lazy" src="assets/img/code-640.jpg" alt="team">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo laboriosam obcaecati sint iure illo, nisi facilis nulla odit in error dicta sequi doloremque voluptas aliquam officia animi debitis reiciendis?</p>
-                <article>
-                    <h3><a href="#">Front</a></h3>
-                    <img loading="lazy" src="assets/img/code-640.jpg" alt="team">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo laboriosam obcaecati sint iure illo, nisi facilis nulla odit in error dicta sequi doloremque voluptas aliquam officia animi debitis reiciendis?</p>
-                </article>
-                <article>
-                    <h3><a href="#">Back</a></h3>
-                    <img loading="lazy" src="assets/img/code-640.jpg" alt="team">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo laboriosam obcaecati sint iure illo, nisi facilis nulla odit in error dicta sequi doloremque voluptas aliquam officia animi debitis reiciendis?</p>
-                </article>
-            </section>
-            <section>
-                <h2><a href="#">test web dev</a></h2>
-                <img loading="lazy" src="assets/img/team-640.jpg" alt="team">
+            </article>
+            <article>
+                <h3><a href="#">Back</a></h3>
+                <img loading="lazy" src="assets/img/code-640.jpg" alt="team">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo laboriosam obcaecati sint iure illo, nisi facilis nulla odit in error dicta sequi doloremque voluptas aliquam officia animi debitis reiciendis?</p>
-                <article>
-                    <h3><a href="#">test web dev</a></h3>
-                    <img loading="lazy" src="assets/img/code-640.jpg" alt="team">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo laboriosam obcaecati sint iure illo, nisi facilis nulla odit in error dicta sequi doloremque voluptas aliquam officia animi debitis reiciendis?</p>
-                </article>
-            </section>
-        </main>
-        <footer>
-            <p>tous droits réservés</p>
-        </footer>
-        <div id="debug"></div>
-        <script src="assets/uikit/js/uikit.min.js"></script>
+            </article>
+        </section>
+        <section>
+            <h2><a href="#">test web dev</a></h2>
+            <img loading="lazy" src="assets/img/team-640.jpg" alt="team">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo laboriosam obcaecati sint iure illo, nisi facilis nulla odit in error dicta sequi doloremque voluptas aliquam officia animi debitis reiciendis?</p>
+            <article>
+                <h3><a href="#">test web dev</a></h3>
+                <img loading="lazy" src="assets/img/code-640.jpg" alt="team">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo laboriosam obcaecati sint iure illo, nisi facilis nulla odit in error dicta sequi doloremque voluptas aliquam officia animi debitis reiciendis?</p>
+            </article>
+        </section>
+    </main>
+    <footer>
+        <p>tous droits réservés</p>
+    </footer>
+    <div id="debug"></div>
+    <script src="assets/uikit/js/uikit.min.js"></script>
     <script src="assets/uikit/js/uikit-icons.min.js"></script>
 
     <script src="assets/js/vue.global.prod.js"></script>
-        <script>
-            // debug.innerHTML = mypage.width + '/' + mypage.height;
-        </script>
+    <script>
+        // debug.innerHTML = mypage.width + '/' + mypage.height;
+    </script>
     </div>
 </body>
 
