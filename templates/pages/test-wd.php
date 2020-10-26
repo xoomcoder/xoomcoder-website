ce3ec182afc450384154af66c8fbb2392de57910
LH XoomCoder <github@xoomcoder.com>
2020-10-26 21:05:42 +0100

    css image reduce height

--- templates/pages/test-wd.php
+++ templates/pages/test-wd.php
@@ -35,6 +35,11 @@
             font-weight: 700;
         }
 
+        .h200 {
+            width: 100%;
+            height: 200px;
+            object-fit: cover;
+        }
         .h300 {
             width: 100%;
             height: 300px;
@@ -83,12 +88,12 @@
                 <div class="uk-container">
                     <div class="uk-grid uk-child-width-1-2@s" uk-grid>
                         <article>
-                            <img loading="lazy" src="assets/img/team-640.jpg" alt="team" class="h300">
+                            <img loading="lazy" src="assets/img/team-640.jpg" alt="team" class="h200">
                             <h1>Formation Dev FullStack</h1>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo laboriosam obcaecati sint iure illo, nisi facilis nulla odit in error dicta sequi doloremque voluptas aliquam officia animi debitis reiciendis?</p>
                         </article>
                         <article>
-                            <img loading="lazy" src="assets/img/team-640.jpg" alt="team" class="h300">
+                            <img loading="lazy" src="assets/img/team-640.jpg" alt="team" class="h200">
                             <h2>Formation à distance</h2>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quo laboriosam obcaecati sint iure illo, nisi facilis nulla odit in error dicta sequi doloremque voluptas aliquam officia animi debitis reiciendis?</p>
                         </article>
